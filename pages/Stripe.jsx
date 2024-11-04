2711747f1e456ec38a59a7a1401c67b546ecd3db
wendywen20 <wenyimei6699@gmail.com>
2024-11-04 12:02:16 -0500

    Stripe API

--- pages/Stripe.jsx
+++ pages/Stripe.jsx
@@ -9,23 +9,33 @@ export default function CheckoutButton() {
 const { cart } = useContext(CartContext); 
 const totalPrice = cart.reduce((total, item) => total + item.price, 0);
     // Function to handle the checkout session
-    const handleCheckout = async(e) => {
+    //set destructure here from cart in the req.body
+    const {name, _id,price}=cart
+    const handleCheckout = async() => {
 
-      const res=await fetch('http://localhost:3000/create-checkout-session', {
+      fetch('http://localhost:3000/create-checkout-session', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(cart),
+        body: JSON.stringify({name,_id, price}
+          // cart
+          // items:[
+          //   {id:1, quantity:3},
+          //   {id:2, quantity:1}
+          // ]
+        ),
       })
         .then((res) => {
             console.log(res)
             console.log(res.body)
+            console.log(cart)
           if (res.ok) return res.json();
-          return res.json().then((json) => Promise.reject(json));
+          return res.json().then(json=>Promise.reject(json))
+          
         })
         .then(({ url }) => {
-           console.log(url)
+          //  console.log(url)
           window.location = url; // Redirect to the checkout URL
         })
         .catch((e) => {
